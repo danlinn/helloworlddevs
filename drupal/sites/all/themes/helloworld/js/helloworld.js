b5da53ed977ddd976359e10e5a9f20e46d6ed1a3
Dan Linn <dan@danlinn.com>
2015-06-08 11:53:10 -0700

    removed skrollr from mobile

--- drupal/sites/all/themes/helloworld/js/helloworld.js
+++ drupal/sites/all/themes/helloworld/js/helloworld.js
@@ -5,7 +5,9 @@
   Drupal.behaviors.scrollHeader = {
     attach: function(context, settings) {
       $('body').once(function(){
-        var s = skrollr.init();
+        if($(window).width() > 600) {
+          var s = skrollr.init();
+        }
       });
       blinkyTimer = setTimeout(blinky(),8000);
     }
@@ -13,22 +15,11 @@
   function blinky() {
     console.log('blinky');
     $('.punctuation').text(':)');
-    //blink1 = setTimeout(function(){
-    //  console.log('1');
-    //  $('.punctuation').text(';)');
-    //},200);
-    //blink2 = setTimeout(function(){
-    //  $('.punctuation').text(':)');
-    //  console.log('2');
-    //},700);
-    blink3 = setTimeout(function(){
+    blink = setTimeout(function(){
       $('.punctuation').text(';)');
-      console.log('3');
-      //clearTimeout(blink1);
-      //clearTimeout(blink2);
       clearTimeout(blinkyTimer);
       blinkyTimer = setTimeout(blinky,5000);
-      clearTimeout(blink3);
+      clearTimeout(blink);
     },300);
   }
 })(jQuery);
